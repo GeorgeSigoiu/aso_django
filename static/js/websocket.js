bd474c889ec96c4cce86845ca1c3fd83846be2b4
George Sigoiu <georgesigoiu1@gmail.com>
2022-12-23 01:33:11 +0200

    update

--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,11 +1,11 @@
-var socket = new WebSocket('http://' + window.location.host + '/ws/socket-server/');
+// var socket = new WebSocket('http://' + window.location.host + '/ws/socket-server/');
 
-console.log("file js imported")
+// console.log("file js imported")
 
-socket.onopen = function open() {
-    console.log('WebSockets connection created.');
-};
+// socket.onopen = function open() {
+//     console.log('WebSockets connection created.');
+// };
 
-if (socket.readyState == WebSocket.OPEN) {
-    socket.onopen();
-}
+// if (socket.readyState == WebSocket.OPEN) {
+//     socket.onopen();
+// }
